0f494c950fe0a58ddd3f4f074a42e04eedf48c5c
dakouan <abdelkrim.dakouan@gmail.com>
2018-12-04 14:05:11 +0100

    form for app

--- application.py
+++ application.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template,url_for,request
+from flask_bootstrap import Bootstrap
+import os,pickle
+from pprint import pprint
+
+plomb=cooper=zinc="Empty"
+
+"""f=open("model4.sav", 'rb')  
+loaded_model = pickle.load(f)"""
+
 app = Flask(__name__)
+Bootstrap(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+def predict(values):
+    X=[]
+    for value in values.items():
+        X.append(value)
+    X=[X]
+    y_predict=loaded_model.predict(X)
+    return y_predict[0][0],y_predict[0][1],y_predict[0][2]
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/",methods=['GET','POST'])
+def index():
+    return render_template("index.html")
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = "empty","empty","empty"#predict(request.form)
+      return render_template("result.html",result = result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
